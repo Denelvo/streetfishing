384f3e5766f6575ea028779113021988f92a0c4f
u46041 <bruno.heremans@kbc.be>
2021-10-07 10:33:53 +0200

    multiple files

--- upload_pic.py
+++ upload_pic.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from os import path
 app = Flask(__name__)
 
+@app.route('/multiupload', methods=['GET','POST'])
+def upload():
+    if request.method == "POST":
+        files = request.files.getlist("file")
+        for file in files:
+            file.save(path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+
 @app.route('/upload')
-def upload_file():
+def screen_upload_file():
     return render_template('upload.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
-        f.save(secure_filename(f.filename))
+        f.save("uploads/" + f.filename)
         return 'file uploaded successfully'
 
 if __name__ == '__main__':
